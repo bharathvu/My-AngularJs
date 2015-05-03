f4346343e30cf1c732a17a285aadfe8930453684
Bharath Upadhya <bharathvu@gmail.com>
2015-05-03 09:27:39 +0400

    changes...

--- search.js
+++ search.js
@@ -1,8 +1,24 @@
-var ang = angular.module('bharath-directive', ['ui.bootstrap']);
-ang.directive('countrySearch', function () {
-    return {
-        restrict: 'E',
-        templateUrl: 'search.html',
-        replace: true
-    };
-});
+'use strict';
+
+angular.module('bharath-directive', ['ui.bootstrap', 'ui.bootstrap.typeahead'])
+    .directive('countrySearch', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'bower_components/bharath-directives/search.html',
+            replace: true,
+            controller: function ($scope, $http) {
+                $scope.getLocation = function (val) {
+                    return $http.get('http://maps.googleapis.com/maps/api/geocode/json', {
+                        params: {
+                            address: val,
+                            sensor: false
+                        }
+                    }).then(function (response) {
+                        return response.data.results.map(function (item) {
+                            return item.formatted_address;
+                        });
+                    });
+                };
+            }
+        };
+    });
